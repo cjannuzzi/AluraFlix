fd0a41f5237987aed61fe3857b2ab5c0d9cf3f49
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-16 14:03:19 -0300

    Atualizar arquivo JS.

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 var listaFilmes = ["Yesterday", "A chegada", "Escola de Rock"]
 listaFilmes.push("Harry Potter 2")
 
-// var filme1 = "Yesterday";
-// var filme2 = "A chegada";
-// var filme3 = "Escola de Rock";
-
-
-//experimentar o h1, h2 e strong
-// document.write("<p>" + listaFilmes[0] + "</p>");
-// document.write("<p>" + listaFilmes[1] + "</p>");
-// document.write("<p>" + listaFilmes[2] + "</p>");
-// document.write("<p>" + listaFilmes[3] + "</p>");
-
 for (var indice = 0; indice < 4; indice = indice +1 ){
     document.write("<p>" + listaFilmes[indice] + "</p>");
 }
